dcec2b073e4c0c52b00f4baeea21681de2559732
Mark Rabey <mark@markrabey.com>
2018-04-27 10:44:11 -0400

    fix linting errors

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,25 @@ import { promisify } from 'util';
 
 const createDatastore = (datastore: NeDB) => {
   const asyncDatastore: any = { nedb: datastore };
-  const asyncMethods = ['loadDatabase', 'insert', 'find', 'findOne', 'count', 'update', 'remove', 'ensureIndex', 'removeIndex'];
+  const asyncMethods = [
+    'loadDatabase',
+    'insert',
+    'find',
+    'findOne',
+    'count',
+    'update',
+    'remove',
+    'ensureIndex',
+    'removeIndex'
+  ];
 
   asyncMethods.forEach(method => {
     asyncDatastore[method] = promisify(datastore[method].bind(datastore));
   });
 
   return asyncDatastore;
-}
+};
 
 export const Datastore = (options: NeDB.DataStoreOptions) => {
   return createDatastore(new NeDB(options));
-}
+};
