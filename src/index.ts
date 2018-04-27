7279b71bb936d6da941808bfa8e54fd69db1cc71
Mark Rabey <mark@markrabey.com>
2018-04-27 10:37:55 -0400

    add code

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import * as NeDB from 'nedb';
+import { promisify } from 'util';
+
+const createDatastore = (datastore: NeDB) => {
+  const asyncDatastore: any = { nedb: datastore };
+  const asyncMethods = ['loadDatabase', 'insert', 'find', 'findOne', 'count', 'update', 'remove', 'ensureIndex', 'removeIndex'];
+
+  asyncMethods.forEach(method => {
+    asyncDatastore[method] = promisify(datastore[method].bind(datastore));
+  });
+
+  return asyncDatastore;
+}
+
+export const Datastore = (options: NeDB.DataStoreOptions) => {
+  return createDatastore(new NeDB(options));
+}
